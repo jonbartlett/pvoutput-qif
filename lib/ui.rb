9d73d10ddc7139f0ea9df14ed66fa00d06467706
Jon Bartlett <mail@jaybe.net mailto:mail@jaybe.net>
2015-08-26 15:36:44 +1000

    add ui

---
+++ lib/ui.rb
@@ -0,0 +1,25 @@
+require 'singleton'
+
+module UI
+
+  class Progress
+    include Singleton
+
+    def self.do_something (counter, total)
+      print "\r  Comparing files.... #{counter} of #{total}: #{(counter / total)*100 }%"
+      print "\n" if counter == total 
+    end
+
+  end
+
+  class Banner
+    include Singleton
+
+    def self.print
+      puts ""
+      puts " PVOuput Qif Exporter"
+      puts " https://github.com/jonbartlett/pvoutput-qif"
+    end
+  end
+
+end
